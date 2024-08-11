d13d72c625b8278000715dd85f123f6a1b426484
fomaxdm <dmont03@mail.ru>
2024-08-11 14:04:37 +0300

    123

---
+++ sql/dsd.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Trip(
+    trip_id int PRIMARY KEY,
+    company int,
+    plane varchar,
+    town_from VARCHAR(30),
+    town_to VARCHAR(30),
+    time_out DATETIME,
+    time_in DATETIME 
+)
+
+
+~
