a158e1c06df83da940decd624219825d1112741a
fomaxdm <dmont03@mail.ru>
2024-08-16 13:38:03 +0300

    16.08.24

--- sql/dsd.sql
+++ sql/dsd.sql
@@ -1,12 +1,24 @@
 CREATE TABLE Trip(
     trip_id int PRIMARY KEY,
     company int,
-    plane varchar,
+    plane varchar(30),
     town_from VARCHAR(30),
     town_to VARCHAR(30),
     time_out DATETIME,
     time_in DATETIME 
 )
+CREATE TABLE company(
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
+CREATE TABLE Pass_in_trip(
+    id INT PRIMARY KEY,
+    trip INT,
+    passenger INT,
+    place VARCHAR(30)
+)
+CREATE TABLE Passenger(
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
 
-
-~
